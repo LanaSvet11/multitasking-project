3eb4586d9291912337f89c309486aa6d970558a1
Svitlana Holovatenko <sv.golovatenko@gmail.com>
2023-09-03 14:02:38 -0700

    first-commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const hourE1 = document.getElementById("hour");
+const minuteE1 = document.getElementById("minutes");
+const secondE1 = document.getElementById("seconds");
+const ampmE1 = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hourE1.innerText = h;
+  minuteE1.innerText = m;
+  secondE1.innerText = s;
+  ampmE1, (innerText = ampm);
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
